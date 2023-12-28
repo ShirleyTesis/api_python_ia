38045a00b07e32fb1665f766063e29a01cdeac3c
ShirleyTesis <155035857+ShirleyTesis@users.noreply.github.com>
2023-12-27 23:06:31 -0500

    Update 2 api.py

--- api.py
+++ api.py
@@ -1,10 +1,33 @@
-from flask import Flask
+from flask import Flask, jsonify
+import requests
+import pandas as pd
+import joblib
+import numpy as np
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/predict', methods=['GET'])
+def predict_stress():
+    # Tu código para obtener datos y realizar predicciones
+    # (Simulando el comportamiento actual con datos estáticos)
+    url_php_script = 'https://stressappshirleybd.000webhostapp.com/model.php'
+    response = requests.get(url_php_script)
+    data = response.json()
+    df_combined = pd.DataFrame(data, index=[0])
+    
+    modelo = joblib.load('Random Forest_model.pkl')
+    predicciones = modelo.predict(df_combined.values.reshape(1, -1))
+
+    # Mapeo de predicciones a respuestas
+    if np.any(predicciones == 0):
+        resultado = "Estres bajo"
+    elif np.any(predicciones == 1):
+        resultado = "Estres moderado"
+    else:
+        resultado = "Estres alto"
+
+    # Devolver el resultado como JSON
+    return jsonify({"resultado": resultado})
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
